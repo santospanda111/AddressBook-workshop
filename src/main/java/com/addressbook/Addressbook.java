e839837e061b44d5c0a24d8f1d9c989fb8abd66b
santospanda111 <santospanda111@gmail.com>
2021-06-03 11:28:35 +0530

    Added welcome message

---
+++ src/main/java/com/addressbook/Addressbook.java
@@ -0,0 +1,11 @@
+package com.addressbook;
+
+public class Addressbook {
+    /**
+     * Added welcome message
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook System");
+    }
+}
